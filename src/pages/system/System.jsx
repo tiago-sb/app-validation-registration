43d88f449be5ba1dca07ba7cbc94e74ed445e7d5
tiago-sb <belatiago72@gmail.com>
2025-06-24 14:44:48 -0300

    modificações finais

--- src/pages/system/System.jsx
+++ src/pages/system/System.jsx
@@ -1,16 +1,23 @@
 import React from "react"
 import { Container } from "react-bootstrap"
-import {  Title } from "../../components/Login/styles";
+import { Title } from "../../components/Login/styles"
+import { Button } from '../../components/Button'
+import { Link } from 'react-router-dom'
 
 const System = () => {
   return (
-    <Container style={{ marginTop: 100 }}
-      className='shadow-lg p-3 mb-5 bg-white rounded'
-    >
-      <Title>
-        Logado!
-      </Title>
-    </Container>
+    <>
+      <Container style={{ marginTop: 100 }}
+        className='shadow-lg p-3 mb-5 bg-white rounded'
+      >
+        <Title>
+          Logado!
+        </Title>
+      </Container>
+      <Link to="/">
+        <Button title='Sair' disabled={true} type="button" />
+      </Link>
+    </>
   )
 }
 
